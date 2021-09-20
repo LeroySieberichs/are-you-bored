d1a3b1bf730d473060bca1440997dfc2f15a0b74
Leroy Sieberichs <lsieberichs@gmail.com>
2021-09-20 13:27:00 +0200

    Changed card style to be bigger

--- src/components/wireframe/DummyRefresh.tsx
+++ src/components/wireframe/DummyRefresh.tsx
@@ -2,7 +2,7 @@ import { firestore } from "../../lib/firebase";
 import React, { useState, useEffect } from 'react';
 import { useCollectionDataOnce } from 'react-firebase-hooks/firestore';
 import Distraction from '../Distraction'
-import { Box, chakra, Flex, useColorModeValue, Image, useColorMode, Button, SkeletonText, SkeletonCircle, Container, Skeleton, Link } from "@chakra-ui/react";
+import { Box, chakra, Flex, useColorModeValue, Image, useColorMode, Button, SkeletonText, SkeletonCircle, Container, Skeleton, Link, Badge, Input, InputGroup, InputRightElement, SimpleGrid, VisuallyHidden } from "@chakra-ui/react";
 import '../../style/card.css'
 
 
@@ -57,7 +57,73 @@ export default function DummyRefresh(props: { NextItem: any; EndDistractions: bo
     
   }
 
-  const distractionCard = (data: { name: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; description: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; image: string | undefined; id: any }) => {
+  const distractionCard = (data: { id: React.Key | null | undefined; acceptedLink: { acceptedLink: string; }; image: string | undefined; name: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; description: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; }) => {
+    return (
+<SimpleGrid
+      className={props.NextItem == data.id ? 'animate__animated animate__lightSpeedInRight distraction' : props.NextItem > data.id ? 'animate__animated animate__lightSpeedOutLeft distraction' : 'inactive'}
+      key={data.id}
+      columns={{ base: 1, md: 2 }}
+      spacing={0}
+      _after={{
+        bg:  "white",
+        opacity: 0.25,
+        pos: "absolute",
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        zIndex: -1,
+        content: '" "',
+      }}
+    >
+      <Flex
+        direction="column"
+        alignItems="start"
+        rounded="lg"
+        shadow="lg"
+        justifyContent="center"
+        px={{ base: 4, lg: 20 }}
+        py={24}
+        
+      >
+        
+        <chakra.h1
+          mb={6}
+          fontSize={{ base: "4xl", md: "4xl", lg: "5xl" }}
+          fontWeight="bold"
+          color={colorMode}
+          lineHeight="shorter"
+        >
+          {data.name}
+        </chakra.h1>
+        <chakra.p
+          pr={{ base: 0, lg: 16 }}
+          mb={4}
+          fontSize="sm"
+          color={colorMode}
+          letterSpacing="wider"
+        >
+         {data.description}
+        </chakra.p>
+      </Flex>
+      <Box>
+        <Image
+          src={data.image}
+          alt="logo"
+          fit="cover"
+          w="full"
+          h={{ base: 64, md: "full" }}
+          bg="gray.100"
+          loading="lazy"
+        />
+      </Box>
+    </SimpleGrid>
+    )
+  }
+
+
+
+  const distractionCardOld = (data: { id: React.Key | null | undefined; acceptedLink: { acceptedLink: string; }; image: string | undefined; name: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; description: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; }) => {
     return (
       <Box className={props.NextItem == data.id ? 'animate__animated animate__lightSpeedInRight distraction' : props.NextItem > data.id ? 'animate__animated animate__lightSpeedOutLeft distraction' : 'inactive'}
         key={data.id}
@@ -108,7 +174,7 @@ export default function DummyRefresh(props: { NextItem: any; EndDistractions: bo
   }
 
   return (
-    <Container h="21em" centerContent className="outer">
+    <Container h="40em"maxW="container.xl" centerContent className="outer">
       {error && <strong>Error: {JSON.stringify(error)}</strong>}
       {loading && skeleton()}
       {value && cardGenerate(value)}
