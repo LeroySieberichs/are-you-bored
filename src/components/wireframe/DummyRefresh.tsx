f07a2fe43645606d23c853c89909b6226ae216f0
Leroy Sieberichs <lsieberichs@gmail.com>
2021-12-13 12:58:48 +0100

    optimization

--- src/components/wireframe/DummyRefresh.tsx
+++ src/components/wireframe/DummyRefresh.tsx
@@ -16,6 +16,7 @@ export default function DummyRefresh(props: { NextItem: any; EndDistractions: bo
 
   let itemlist: JSX.Element[] = []
 
+  // retrieves first 10 entires in the firestore document
   const [value, loading, error] = useCollectionDataOnce(
     firestore.collection('distractions').limit(10),
     {
@@ -24,19 +25,16 @@ export default function DummyRefresh(props: { NextItem: any; EndDistractions: bo
   );
 
 
-
+  // uses the given data to create cards and pushes them to a list of cards, which will then be returned
   function cardGenerate(data: any[]) {
     data.forEach((item: any) => {
-
       itemlist.push(distractionCard(item))
-
-
-
     })
 
     return itemlist;
   }
 
+  // small animation for when data is loading (glowing grey boxes)
   const skeleton = () => {
     return (
       <Box padding="10" boxShadow="lg" bg="white">
@@ -47,134 +45,84 @@ export default function DummyRefresh(props: { NextItem: any; EndDistractions: bo
 
   }
 
+  // turns the link into a clickable href
   const notBored = (data: { acceptedLink: string; }) => {
-    console.log(data);
-    
-    if (props.EndDistractions === true) 
-    
-    window.location.href = data;
 
-    
+    if (props.EndDistractions === true)
+
+      window.location.href = data;
+
+
   }
 
+
+  //big function of generating cards out of given data from the firestore document
   const distractionCard = (data: { id: React.Key | null | undefined; acceptedLink: { acceptedLink: string; }; image: string | undefined; name: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; description: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; }) => {
     return (
-<SimpleGrid
-      className={props.NextItem == data.id ? 'animate__animated animate__lightSpeedInRight distraction' : props.NextItem > data.id ? 'animate__animated animate__lightSpeedOutLeft distraction' : 'inactive'}
-      key={data.id}
-      columns={{ base: 1, md: 2 }}
-      spacing={0}
-      _after={{
-        bg:  "white",
-        opacity: 0.25,
-        pos: "absolute",
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-        zIndex: -1,
-        content: '" "',
-      }}
-    >
-      <Flex
-        direction="column"
-        alignItems="start"
-        rounded="lg"
-        shadow="lg"
-        justifyContent="center"
-        px={{ base: 4, lg: 20 }}
-        py={24}
-        
-      >
-        
-        <chakra.h1
-          mb={6}
-          fontSize={{ base: "4xl", md: "4xl", lg: "5xl" }}
-          fontWeight="bold"
-          color={colorMode}
-          lineHeight="shorter"
-        >
-          {data.name}
-        </chakra.h1>
-        <chakra.p
-          pr={{ base: 0, lg: 16 }}
-          mb={4}
-          fontSize="sm"
-          color={colorMode}
-          letterSpacing="wider"
-        >
-         {data.description}
-        </chakra.p>
-      </Flex>
-      <Box>
-        <Image
-          src={data.image}
-          alt="logo"
-          fit="cover"
-          w="full"
-          h={{ base: 64, md: "full" }}
-          bg="gray.100"
-          loading="lazy"
-        />
-      </Box>
-    </SimpleGrid>
-    )
-  }
-
-
-
-  const distractionCardOld = (data: { id: React.Key | null | undefined; acceptedLink: { acceptedLink: string; }; image: string | undefined; name: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; description: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; }) => {
-    return (
-      <Box className={props.NextItem == data.id ? 'animate__animated animate__lightSpeedInRight distraction' : props.NextItem > data.id ? 'animate__animated animate__lightSpeedOutLeft distraction' : 'inactive'}
+      <SimpleGrid
+        className={props.NextItem == data.id ? 'animate__animated animate__lightSpeedInRight distraction' : props.NextItem > data.id ? 'animate__animated animate__lightSpeedOutLeft distraction' : 'inactive'}
         key={data.id}
-        w="xs"
-        bg={colorMode}
-        shadow="lg"
-        rounded="lg"
-        overflow="hidden"
-        mx="auto"
+        columns={{ base: 1, md: 2 }}
+        spacing={0}
+        _after={{
+          bg: "white",
+          opacity: 0.25,
+          pos: "absolute",
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          zIndex: -1,
+          content: '" "',
+        }}
       >
-        {props.NextItem == data.id? notBored(data.acceptedLink): ''}
-        <Box
-          w="xs"
-          bg={colorMode}
-          shadow="lg"
+        <Flex
+          direction="column"
+          alignItems="start"
           rounded="lg"
-          overflow="hidden"
-          mx="auto"
+          shadow="lg"
+          justifyContent="center"
+          px={{ base: 4, lg: 20 }}
+          py={24}
+
         >
+
+          <chakra.h1
+            mb={6}
+            fontSize={{ base: "4xl", md: "4xl", lg: "5xl" }}
+            fontWeight="bold"
+            color={colorMode}
+            lineHeight="shorter"
+          >
+            {data.name}
+          </chakra.h1>
+          <chakra.p
+            pr={{ base: 0, lg: 16 }}
+            mb={4}
+            fontSize="sm"
+            color={colorMode}
+            letterSpacing="wider"
+          >
+            {data.description}
+          </chakra.p>
+        </Flex>
+        <Box>
           <Image
-            w="full"
-            h={56}
-            fit="cover"
             src={data.image}
-            alt="avatar"
+            alt="logo"
+            fit="cover"
+            w="full"
+            h={{ base: 64, md: "full" }}
+            bg="gray.100"
+            loading="lazy"
           />
-
-          <Box py={5} textAlign="center">
-            <Link
-
-              display="block"
-              fontSize="2xl"
-              color={colorMode}
-              fontWeight="bold"
-            >
-              {data.name}
-            </Link>
-            <chakra.span
-              fontSize="sm"
-              color={colorMode}
-            >
-              {data.description}
-            </chakra.span>
-          </Box>
         </Box>
-      </Box>
+      </SimpleGrid>
     )
   }
 
   return (
-    <Container h="40em"maxW="container.xl" centerContent className="outer">
+    <Container h="40em" maxW="container.xl" centerContent className="outer">
       {error && <strong>Error: {JSON.stringify(error)}</strong>}
       {loading && skeleton()}
       {value && cardGenerate(value)}
